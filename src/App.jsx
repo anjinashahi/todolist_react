8f4da86a4fe19e38d813a26a16eaaf98ee4e73aa
Anjina Shahi <np03cs4a220179@heraldcollege.edu.np>
2023-09-22 18:48:12 +0545

    last

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,26 @@ export default function App() {
       return[...currentTodos, 
       {id: crypto.randomUUID(), title: newItem, completed:false },]})
     
+      setNewItem("")
   }
+
+  function toggleTodo(id, completed){
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id){
+          return{ ...todo, completed}
+        }
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id){
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
   
   return(
   <>
@@ -19,20 +38,25 @@ export default function App() {
     <div className = "form-row">
       <label htmlFor>New Item</label>
       <input value = {newItem} 
-      onChange = {e=> setNewItem(e.target.valuetype)} 
+      onChange = {e=> setNewItem(e.target.value)} 
       type = "text" id="item"></input>
     </div>
     <button className ="btn">Add</button>
   </form>
   <h1 className = "header">Todo List</h1>
   <ul className = "list">
+    {todos.length === 0 && "No Todos"}
     {todos.map(todo =>{
-      return  <li>
+      return  <li key = {todo.id}>
       <label>
-        <input type = "checkbox" checked ={todo.completed}></input>
+        <input type = "checkbox" checked ={todo.completed}
+                  onChange = {e => toggleTodo(todo.id, e.target.checked)}>
+        </input>
         {todo.title}
       </label>
-      <button className = "btn btn-danger">Delete</button>
+      <button onClick = {() => deleteTodo(todo.id)} className = "btn btn-danger">
+        Delete
+      </button>
     </li>
     })}
   </ul>
